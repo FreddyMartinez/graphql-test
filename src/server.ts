f9a5c85d523d5defaa253944f2a5fbc4af1664fd
Freddy Martínez <fredylucasrp@gmail.com>
2021-06-08 20:38:14 +0200

    Organizing code

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,11 @@
 import express from "express";
 import compression from "compression";
-import { IResolvers } from "@graphql-tools/utils";
-import { GraphQLSchema } from "graphql";
-import { makeExecutableSchema } from "@graphql-tools/schema";
 import { graphqlHTTP } from 'express-graphql'
+import schema from "./schema";
 
 const app = express();
 
 app.use(compression());
-
-const typeDefs = `
-  type Query {
-    hola: String!
-    holaConNombre(nombre: String!): String!
-    holaGraphQL: String!
-  }
-`;
-
-const resolvers: IResolvers = {
-  Query : {
-    hola(): string{
-      return 'Hola Mundo';
-    },
-    holaConNombre(__: void, { nombre }): string{
-      return `Hola ${nombre}`;
-    },
-    holaGraphQL(): string {
-      return 'Hola GraphQL';
-    }
-  }
-}
-
-const schema: GraphQLSchema = makeExecutableSchema({typeDefs, resolvers});
 
 app.use("/", graphqlHTTP({
   schema,
